4b400dc3089bff0178a9bd670bc84eaac13f5356
Hafiz tech <abdulhafiz99888@gmail.com>
2025-06-11 21:07:45 +0100

    Made changes in the sidemenu

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname){
-  for(tablink of tablinks){
-    tablink.classList.remove("active-link");
+  for(tablinks of tablinks){
+    tablinks.classList.remove("active-link");
   }
 
-  for(tabcontent of tabcontents){
-    tabcontent.classList.remove("active-tab");
+  for(tabcontents of tabcontents){
+    tabcontents.classList.remove("active-tab");
   }
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab")
@@ -20,7 +20,7 @@ function openmenu(){
 }
 
 function closemenu(){
-  sidemenu.style.right = "-200px";
+  sidemenu.style.right = "-100px";
 }
 
 // Add event listeners for mobile menu toggle
