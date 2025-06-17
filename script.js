e3ea8cf3d584717f7d901f31e197a12630e428b3
Hafiz tech <abdulhafiz99888@gmail.com>
2025-06-17 19:09:52 +0100

    Resolved some issues with reponsiveness

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-  for(tablinks of tablinks){
+function opentab(tabname) {
+  for (tablinks of tablinks) {
     tablinks.classList.remove("active-link");
   }
 
-  for(tabcontents of tabcontents){
+  for (tabcontents of tabcontents) {
     tabcontents.classList.remove("active-tab");
   }
   event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab")
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
 var sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
+function openmenu() {
   sidemenu.style.right = "0"; // show the menu
   document.querySelector(".fa-bars").style.display = "none"; // hide hamburger
   document.querySelector(".fa-times").style.display = "block"; // show close icon
 }
 
-function closemenu(){
-    sidemenu.style.right = "-200px"; // hide the menu
+function closemenu() {
+  sidemenu.style.right = "-200px"; // hide the menu
   document.querySelector(".fa-bars").style.display = "block"; // show hamburger
   document.querySelector(".fa-times").style.display = "none"; // hide close icon
 }
 
 // Add event listeners for mobile menu toggle
-document.querySelector(".fa-bars").addEventListener("click", openmenu);
-document.querySelector(".fa-times").addEventListener("click", closemenu);
+// document.querySelector(".fa-bars").addEventListener("click", openmenu);
+// document.querySelector(".fa-times").addEventListener("click", closemenu);
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwgkNdhuYSX3y1IT4yZAqPyAwcnsJ7fWsa6vFdRGkVbaZhjG0ZSwHLeBN58q3Q_ove8/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwgkNdhuYSX3y1IT4yZAqPyAwcnsJ7fWsa6vFdRGkVbaZhjG0ZSwHLeBN58q3Q_ove8/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  message.innerHTML = "Message Sending..."
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      msg.innerHTML = "Messsage Sent successfully"
-      setTimeout(function(){
-        msg.innerHTML = ""
-      },5000)
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  message.innerHTML = "Message Sending...";
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Messsage Sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
 
-      form.reset()
+      form.reset();
     })
-    .catch(error => console.error('Error!', error.message))
-})
+    .catch((error) => console.error("Error!", error.message));
+});
